2a40a454c9a8f80d09975a1922e7c80a766f754c
hungrated <zhang295415658@qq.com>
2018-04-02 16:06:51 +0800

    master: init

---
+++ src/components/Example.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './example.less'
+
+const CountApp = ({ count, dispatch }) => {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.record}>Highest Record: {count.record}</div>
+      <div className={styles.current}>{count.current}</div>
+      <div className={styles.button}>
+        <button onClick={() => { dispatch({type: 'count/add'}); }}>+</button>
+      </div>
+    </div>
+  );
+};
+
+Example.propTypes = {};
+
+export default CountApp;
---
+++ src/models/example.js
@@ -0,0 +1,10 @@
+export default {
+
+  namespace: 'count',
+
+  state: {
+    record: 0,
+    current: 0,
+  },
+
+};
